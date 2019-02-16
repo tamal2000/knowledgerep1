423ab7078878c0c28fa132b8f6c449ccc052f570
Mahsa0M <Anana5Anana5>
2019-02-16 19:11:49 +0100

    Still need to work on splitting and the main part

--- SAT_Solver_pt1.py
+++ SAT_Solver_pt1.py
@@ -46,41 +46,75 @@ def simplify(puzzle,FinalSoln,tautology_switch, pure_switch, unit_switch):
     :param unit_switch: puts clauses of size one to true
     :return:
     """
-    puzzle_history = puzzle.copy()  # to keep track of back-tracking
 
-    var_list = []   #for pure literal simplificatoin
-    for clause in puzzle:
-        if unit_switch == 1:
-            if len(clause) == 1:
-                for var in clause:
-                    if var<0:
+    stop_simplifying_check = 1
+    while stop_simplifying_check == 1:   #stops when nothing has changed in the last simplification
+        var_list = []   # for pure literal simplification
+        for clause in puzzle:
+            for var in clause:
+                # tautology
+                if tautology_switch == 1:
+                    if -1 * var in clause:
+                        puzzle.remove(clause)
+                        stop_simplifying_check = 0
+                # unit clause
+                if unit_switch == 1:
+                    if len(clause) == 1:
+                        if var < 0:
+                            FinalSoln[abs(var)] = -1
+                        else:
+                            FinalSoln[var] = 1
+                        puzzle.remove(clause)
+                        stop_simplifying_check = 0
+                # pure
+                if pure_switch == 1:
+                    var_list.append(var)
+
+        if pure_switch == 1:
+            for var in var_list:
+                if -1 * var not in var_list:
+                    stop_simplifying_check = 0
+                    if var < 0:
                         FinalSoln[abs(var)] = -1
                     else:
                         FinalSoln[var] = 1
-        for var in clause:
-            if tautology_switch ==1:
-                if -1 * var in clause:
-                    puzzle.remove(clause)
-            if pure_switch == 1:
-                var_list.append(var)
+        # omitting the assigned variables from clauses and empty clauses
+        for clause in puzzle:
+            if len(clause) == 0:
+                puzzle.remove(clause)
+                stop_simplifying_check = 0
+            for var in clause:
+                if FinalSoln[abs(var)] != 0:
+                    clause.remove(var)
+                    stop_simplifying_check = 0
 
-    if pure_switch == 1:
-        for var in var_list:
-            if -1 * var not in var_list:
-                if var < 0:
-                    FinalSoln[abs(var)] = -1
-                else:
-                    FinalSoln[var] = 1
-
-    return puzzle_history, FinalSoln, puzzle
+    return FinalSoln, puzzle
 
 # ----------Splitting----------
-def split(FinalSoln,Heuristic):
-    if Heuristic == 'random':
-        split_var = random.choice(list(FinalSoln.keys()))
-        split_val = random.randint(0,1)
-    FinalSoln[split_var] = split_val
-    return FinalSoln, split_val, split_var
+def split(FinalSoln, chosen_list, Heuristic):
+    # we can only split those that are not assigned yet
+    vars_to_split = []
+    for var in FinalSoln:
+        if FinalSoln[var] != 0:
+            vars_to_split.append(var)
+
+    backtrack_to = 0    # backtrack_to =0: there is nothing to backtrack to, another variable is chosen in the same branch
+    if len(vars_to_split) == 0: # no split options left, have to back track
+        for var in reversed(chosen_list):
+            if FinalSoln[var] == 1:
+                FinalSoln[var] = -1
+                backtrack_to = var
+                break
+        if backtrack_to == 0:
+            backtrack_to = -1   # the puzzle is inconsistent
+    else:
+        if Heuristic == 'random':
+            split_var = random.choice(vars_to_split)
+
+        FinalSoln[split_var] = 1
+
+
+    return FinalSoln, split_var, backtrack_to
 
 # ----------Stopping----------
 def stop(puzzle,FinalSoln):
@@ -96,32 +130,28 @@ def stop(puzzle,FinalSoln):
     return found_solution
 #add inconsistant
 
-# ----------Back Tracking----------
-def back_track(puzzle_history,FinalSoln,split_val,split_var):
-    # changing the truth value
-    FinalSoln[split_var] = -1*split_val
-    # backtracking in puzzle
-    puzzle = puzzle_history
-
-    return puzzle,FinalSoln
-
-#add other types of backtracking
 
 # ----------Main----------
 tautology_switch = 1
 pure_switch = 1
 unit_switch = 1
 Heuristic = 'random'
-puzzle_history, FinalSoln, puzzle = simplify(puzzle, FinalSoln, tautology_switch, pure_switch, unit_switch)
+FinalSoln, puzzle = simplify(puzzle, FinalSoln, tautology_switch, pure_switch, unit_switch)
 stop_check = stop(puzzle, FinalSoln)
 tautology_switch = 0
 a=1
 while stop_check == 0:
-    FinalSoln,split_val,split_var = split(FinalSoln, Heuristic)
-    puzzle_history, FinalSoln, puzzle = simplify(puzzle, FinalSoln, tautology_switch, pure_switch, unit_switch)
+    chosen_list = []
+    FinalSoln, split_var, backtrack_to = split(FinalSoln, chosen_list, Heuristic)
+    puzzle_history = puzzle.copy()  # to keep track of back-tracking
+    FinalSoln_history = FinalSoln.copy()
+    if backtrack_to == 0:
+        FinalSoln, puzzle = simplify(puzzle, FinalSoln, tautology_switch, pure_switch, unit_switch)
+    else:
+
     stop_check = stop(puzzle, FinalSoln)
     if stop_check == 0:
-        puzzle, FinalSoln = back_track(puzzle_history,FinalSoln,split_val,split_var)
+        puzzle, FinalSoln = back_track(puzzle_history,FinalSoln,split_var)
     print('iter',a)
     print(len(puzzle))
     a +=1
